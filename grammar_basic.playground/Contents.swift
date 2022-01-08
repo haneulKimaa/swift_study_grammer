26017e9b90f7090ff7764dae7470495142a01a65
khnpoq@naver.com <khnpoq@naver.com>
2022-01-08 20:17:41 +0900

    옵셔널 정리 및 enum, 에러처리 추가

--- grammar_basic.playground/Contents.swift
+++ grammar_basic.playground/Contents.swift
@@ -58,14 +58,31 @@ optionalString = "olive"
 print(optionalString) // Optional("olive") -> 연산 안됨
 //print(optionalString+notOptionalString) // Optional("olive") -> 오류(언랩핑 필요)
 
-// 옵셔널 바인딩 : 옵셔널타입은 바로 사용할 수 없음(바로 연산, 출력 불가능)
-// 3가지 방법 존재(1,2 뉘앙스 다름)
-// 1. if let : 값이 있으면 가져와라
+// 옵셔널을 다루는 다섯 가지 방법
+// 옵셔널타입은 바로 사용할 수 없음(바로 연산, 출력 불가능)
+// 1. 옵셔널 체이닝
+// 완전히 옵셔널을 벗겨내지 않고 접근할 때 사용
+struct Developer {
+    var name: String
+}
+struct Company {
+    var name: String
+    var developer: Developer?
+}
+var developer = Developer(name: "Mark")
+var company = Company(name: "Olive", developer: developer)
+print(company.developer?.name) // Optional("Mark")
+// 강제 언랩핑
+print(company.developer!.name) // Mark
+
+// 2. Optional Binding : 옵셔널 바인딩
+// (1,2 뉘앙스 다름)
+// 2-1. if let : 값이 있으면 가져와라
 if let testStr = optionalString {
     print("if let : "+testStr+notOptionalString)
 }
 
-// 2. guard let : 값이 없으면 실행해라.
+// 2-2. guard let : 값이 없으면 실행해라.
 func test() {
     guard let testStr2 = optionalString else {
         return
@@ -73,8 +90,8 @@ func test() {
     print("guard let : "+testStr2+notOptionalString)
 }
 test()
-// 3. 강제 언랩핑
-print("force unwrapping : "+optionalString!+notOptionalString)
+// 3. Force unwrapping : 강제 언랩핑
+print("force unwrapping : " + optionalString! + notOptionalString)
 
 // 4. 묵시적 해제 : 컴파일러에 의한 자동 해제
 // 4-1. 비교연산자를 통해 값을 비교하면 자동으로 변환.
@@ -92,6 +109,10 @@ var stringToInt: Int! = Int(string)
 // 바인딩해야 출력 가능
 print(stringToInt+1)
 
+// 5. 암시적 추출 옵셔널 (Implicitly Unwrapped Optionals)
+// 매번 옵셔널 바인딩하는 것이 불팰요할 때 사용한다.
+// 옵셔널과 같으나 nil이 할당되어있을 때 접근하려고 하면 오류가 난다.
+var owner: Person!
 
 // 구조체
 struct User {
@@ -229,3 +250,103 @@ runGuard() // 24
 //print("\(a)")
 
 
+// enum
+// 1. 연관된 항목을 나열
+// 2. 새로운 타입처럼 사용 가능해서 class처럼 대문자로 선언
+// 3. 각 항목은 항목의 원시값(rawValue)을 가지게 할 수 있음(특정 타입으로 선언 가능)
+// 다른 언어에서는 Integer타입 원시값만 가질 수 있지만 swift는 String, Character, Number Type 가능
+// 모든 케이스가 동일한 형식을 사용해야함
+enum CompassPoint: String {
+    case north = "북"
+    case south = "남"
+    case east = "동"
+    case west
+}
+var direction: CompassPoint = .north
+direction = .south
+switch direction {
+case .north:
+    print(direction.rawValue)
+case .south:
+    print(direction.rawValue)
+case .east:
+    print(direction.rawValue)
+case .west:
+    print(direction.rawValue)
+}
+// 3-1. 생성자 매개변수로 rawValue를 넘겨 항목을 알 수 있음
+// 없을 경우 nil을 리턴(생성자는 옵셔널)
+let direction2 = CompassPoint(rawValue: "남")
+
+enum Task: Int {
+    case morning, afternoon = 100, evening
+}
+enum Score: Character {
+    case math = "A"
+    case science = "B"
+    // 원시값이 Charater형일 때 모든 항목을 직접 할당해줘야함
+    // case evening
+}
+// 원시값 선언하지 않을 경우 항목명으로 들어감
+// 원시값이 Int형일 경우 0부터. 선언한 값이 있다면 그 이후부터 1씩 올라감
+CompassPoint.west.rawValue // west
+Task.morning.rawValue // 0
+Task.afternoon.rawValue // 100
+Task.evening.rawValue // 101
+
+
+// 4. 연관값(associated value)을 가질 수 있음
+// 항목과 연관된 다른 타입의 값을 저장할 때 사용
+enum PhoneError {
+    case unknown
+    case batteryLow(String, Int)
+    
+}
+let error = PhoneError.batteryLow("배터리가 곧 방전됩니다. ", 5)
+switch error {
+case .batteryLow(let message, let percent):
+    print("\(message)\(percent)퍼센트 입니다.")
+case .unknown:
+    print("알 수 없는 에러입니다.")
+}
+// 배터리가 곧 방전됩니다. 5퍼센트 입니다.
+
+// 에러 처리
+// 프로그램 내에 에러가 발생한 상황에 대응하고 복구하는 과정
+// 런타임에 에러가 발생한 경우
+// 1. 발생(throwing) 2. 감지(catching) 3. 전파 propagaing
+// 4. 조작(manipulating)
+
+// 에러 프로토콜 채택한 열거형 선언
+enum PhoneError2: Error {
+    case unknown
+    case batteryLow(batteryLevel: Int)
+}
+
+// 에러 발생 예상 부분에 throw로 오류를 던짐 -> 오류 발생시 이후 코드는 실행되지 않음
+throw PhoneError2.batteryLow(batteryLevel: 20)
+// Swift에서 오류를 처리하는 방법 4가지
+// 1. 함수에서 발생한 오류를 해당 함수를 호출한 코드에 전파
+func checkPhoneBatteryStatus(batterLevel: Int) throws -> String {
+    guard batterLevel != -1 else {
+        throw PhoneError2.unknown
+    }
+    guard batterLevel >= 20 else {
+        throw PhoneError2.batteryLow(batteryLevel: 20)
+    }
+    return "배터리 상태가 정상입니다."
+}
+// 2. do catch로 해결
+// 오류 발생을 전달받은 코드블록에서 do catch로 오류 처리
+// do에서 오류를 던지고 catch에서 오류를 전달받아 예외처리
+do {
+    try checkPhoneBatteryStatus(batterLevel: -1)
+} catch PhoneError2.unknown{
+    print("알 수 없는 에러")
+} catch PhoneError2.batteryLow(let batteryLabel) {
+    print("배터리 전원 부족 남은 배터리 : \(batteryLabel)")
+} catch {
+    print("그 외 오류 발생 : \(error)")
+}
+// 3. optional 값으로 처리
+// 4. 오류가 나지 않을 것으로 확신
